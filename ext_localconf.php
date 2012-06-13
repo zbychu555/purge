a066497901d9f5f397cbdd1e633e709ce1562eec
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-13 08:20:39 +0000

    Added keys to hook registration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/purge/trunk@63373 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,8 @@
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['purge']);
 
 if($confArr['enablePurgeCalls']) {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearPageCacheEval'][] = 'Tx_Purge_Hooks_Tcemain->clearCacheForListOfUids';
-	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'Tx_Purge_Hooks_Tcemain->clearCacheCmd';
+	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearPageCacheEval']['purge'] = 'Tx_Purge_Hooks_Tcemain->clearCacheForListOfUids';
+	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['purge'] = 'Tx_Purge_Hooks_Tcemain->clearCacheCmd';
 }
 
 if (t3lib_extMgm::isLoaded('realurl')) {
