f4cd2778fec8a5bb65a19b93de3ac42c5e1343f8
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2014-07-29 16:12:40 +0200

    [TASK] Use only front-end domain with purge URLs.
    When using asynchronous purge queue processing with RealUrl cache tables
    the purge URL for a page-id is generated by first looking up in to the
    cache tables. If it is found there then fine, else the URL is generated by
    appending the page-id with the host domain i.e. backend domain. This might
    not be same as front-end domain in some cases. Therefore it makes sense to
    only use the front domain with PURGE requests.
    
    Change-Id: If1cf8b5b05c83c6120a964fab8343d8fbad2fbc8
    Reviewed-on: http://review.typo3.org/31900
    Reviewed-by: Chetan Thapliyal <nospam.ct@gmail.com>
    Tested-by: Chetan Thapliyal <nospam.ct@gmail.com>

--- Classes/Finder/PageId.php
+++ Classes/Finder/PageId.php
@@ -13,6 +13,7 @@ class Tx_Purge_Finder_PageId extends Tx_Purge_Finder_Abstract {
 			$url = array();
 			// url is used as regular expression in varnish, so we have to escape '?'
 			$url['path'] = '.*\\\?.*id=' . $uid;
+			$url['domain'] = t3lib_BEfunc::getViewDomain($pageUid);
 			$urls[] = $url;
 		}
 		return $urls;
